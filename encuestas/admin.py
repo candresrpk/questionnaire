a8009fabc83694e5e6a5a700317a68ad6fb9ea5b
Carlos Reyes <andrespk3000@gmail.com>
2025-03-25 23:10:14 -0500

    modelo de encusta preguntas y respuestas creado

--- encuestas/admin.py
+++ encuestas/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from .models import Encuesta, Pregunta, Opcion, Respuesta
 # Register your models here.
+
+
+@admin.register(Encuesta)
+class EncuestaAdmin(admin.ModelAdmin):
+    list_display = ('encuesta', 'status')
+    list_filter = ('status',)
+    search_fields = ('encuesta',)
+    prepopulated_fields = {'encuesta': ('encuesta',)}
+
+
+@admin.register(Pregunta)
+class PreguntaAdmin(admin.ModelAdmin):
+    list_display = ('pregunta', 'encuesta')
+    list_filter = ('encuesta',)
+    search_fields = ('pregunta',)
+
+
+@admin.register(Opcion)
+class OpcionAdmin(admin.ModelAdmin):
+    list_display = ('opcion', 'pregunta')
+    list_filter = ('pregunta',)
+    search_fields = ('opcion',)
+
+
+@admin.register(Respuesta)
+class RespuestaAdmin(admin.ModelAdmin):
+    list_display = ('encuesta', 'pregunta', 'opcion')
+    list_filter = ('encuesta', 'pregunta', 'opcion')
+    search_fields = ('encuesta', 'pregunta', 'opcion')
--- encuestas/models.py
+++ encuestas/models.py
@@ -1,3 +1,90 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Encuesta(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='encuestas')
+    encuesta = models.CharField(max_length=200)
+
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT
+                              )
+
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Encuesta'
+        verbose_name_plural = 'Encuestas'
+
+    def __str__(self):
+        return self.encuesta
+
+
+class Pregunta(models.Model):
+    pregunta = models.CharField(max_length=200)
+    encuesta = models.ForeignKey(
+        Encuesta, on_delete=models.CASCADE, related_name='preguntas')
+
+    def __str__(self):
+        return self.pregunta
+
+
+class Opcion(models.Model):
+
+    class Categoria(models.TextChoices):
+        MULTICHOICES = 'MC', 'Multichoice'
+        OPEN = 'OP', 'Open'
+        SINGLECHOICE = 'SC', 'Singlechoice'
+
+    pregunta = models.ForeignKey(
+        Pregunta, on_delete=models.CASCADE, related_name='opciones')
+
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    opcion = models.CharField(max_length=200)
+    categoria = models.CharField(
+        max_length=200, choices=Categoria.choices, default=Categoria.SINGLECHOICE)
+
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Opcion'
+        verbose_name_plural = 'Opciones'
+
+    def __str__(self):
+        return self.opcion
+
+
+class Respuesta(models.Model):
+    encuesta = models.ForeignKey(
+        Encuesta, on_delete=models.PROTECT, related_name='encuestas')
+
+    pregunta = models.ForeignKey(
+        Pregunta, on_delete=models.PROTECT, related_name='preguntas')
+
+    opcion = models.ForeignKey(
+        Opcion, on_delete=models.PROTECT, related_name='opciones')
+
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Respuesta'
+        verbose_name_plural = 'Respuestas'
+
+    def __str__(self):
+        return f'{self.encuesta} - {self.pregunta} - {self.opcion.categoria}'
